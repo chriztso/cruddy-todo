099668289c7767804405ddd2206358a7f0fd6ab5
chriztso <christophertso2017@u.northwestern.edu>
2019-01-24 14:59:23 -0800

    Implemented delete single todo in index.js

--- datastore/index.js
+++ datastore/index.js
@@ -9,26 +9,23 @@ var items = {};
 
 exports.create = (text, callback) => {
   counter.getNextUniqueId((err, id) => {
-  items[id] = text;
-  fs.writeFile(`${exports.dataDir}/${id}.txt`, items[id], (err) => {
-     if(err){
-       throw err; 
-     }  
-     callback(null, { id, text });
-  })
-   
+    items[id] = text;
+    fs.writeFile(`${exports.dataDir}/${id}.txt`, items[id], (err) => {
+      if (err) {
+        throw err; 
+      }  
+      callback(null, { id, text });
+    });
   });
 };
 
 exports.readAll = (callback) => {
-
   const dir = "./test/testData";
-
   fs.readdir(dir, (err, files) => {
-    if(err){
+    if (err) {
       callback(null, []);
     }
-    var dataResults = []
+    var dataResults = [];
     files.forEach(file => {
       var data = {};
       data['id'] = file.slice(0, 5); 
@@ -36,7 +33,7 @@ exports.readAll = (callback) => {
       dataResults.push(data); 
     });
     callback(null, dataResults);
-  })
+  });
 
 };
 
@@ -47,12 +44,10 @@ exports.readOne = (id, callback) => {
   fs.readFile(dir, 'utf8', (err, data) => {
     if(err){
       callback(err, null);
+    } else {
+      callback(null,  {'id': id, 'text': data});
     }
-   else{
-    callback(null,  {'id': id, 'text': data});
-
-  } 
- })
+  });
   // if (!text) {
   //   callback(new Error(`No item with id: ${id}`));
   // } else {
@@ -61,24 +56,48 @@ exports.readOne = (id, callback) => {
 };
 
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
+  //read file
+  //save contents
+  //update files
+
+    const dir = `${exports.dataDir}/${id}.txt`;
+    const flag = fs.constants.O_WRONLY | fs.constants.O_TRUNC;
+    fs.writeFile(dir, text, {flag}, (err) => {
+      if (err){
+        callback(err);
+      } else {
+        callback(null, {id, text});
+      }
+    });
+ 
+
+ 
+  // var item = items[id];
+  // if (!item) {
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   items[id] = text;
+  //   callback(null, { id, text });
+  // }
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  
+  const dir = `${exports.dataDir}/${id}.txt`;
+  fs.unlink(dir, (err) => {
+    err ? callback(err) : callback();
+});
+  
+  
+  
+  // var item = items[id];
+  // delete items[id];
+  // if (!item) {
+  //   // report an error if item not found
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   callback();
+  // }
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
